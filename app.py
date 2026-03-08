591525f727d0512a8d74ec55d53bad5c4b65f7ad
kumar99786 <147326244+kumar99786@users.noreply.github.com>
2026-03-08 23:34:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("Astranova DevOps Pipeline Running")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Astranova DevOps Pipeline Running 🚀"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=3000)
