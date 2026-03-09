ae98ee1079b2eeb8c40f0637cf40385996ad32a4
kumar99786 <147326244+kumar99786@users.noreply.github.com>
2026-03-09 15:00:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,5 @@ def home():
     return "Astranova DevOps Pipeline Running 🚀"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000)
+    print("Starting Astranova App...")
+    app.run(host="0.0.0.0", port=3000, debug=False)
